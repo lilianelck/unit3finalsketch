31a2f223cdd5329ee4b5c069d1f514327b2d1229
lilianelck <l.lo1@arts.ac.uk>
2017-01-29 21:20:02 +0000

    Add files via upload

---
+++ unit3finalsketch/sketch.js
@@ -0,0 +1,392 @@
+var r = 0;
+ var b = 255;
+ 
+ function setup() {
+  createCanvas(1000,500)
+}
+ 
+ function draw() {
+   
+  // background
+  r = map(mouseX, 0, 600, 0, 255);
+  r = map(mouseX, 0, 600, 0, 255);
+  background (r, 0, b);
+  // ellipse
+  fill(110, 400, 222);
+  ellipse(mouseX, 200, 60, 60);
+  
+  
+  
+
+  ellipse( 140, 35, 60, 60);
+  c=color('#00FFFF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 50, 35, 60, 60);
+  c=color('#D2691E');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 35, 60, 60);
+  c=color('#FFE4C4');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 35, 60, 60);
+  c=color('#32CD32');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 35, 60, 60);
+  c=color('#C71585');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 500, 35, 60, 60);
+  c=color('#A52A2A');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 590, 35, 60, 60);
+  c=color('#E6E6FA');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 680, 35, 60, 60);
+  c=color('#ADD8E6');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 770, 35, 60, 60);
+  c=color('#E0FFFF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 860, 35, 60, 60);
+  c=color('#20B2AA');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 950,35, 60, 60);
+  c=color('#FFE4E1');
+  fill(c); // Use updated 'c' as fill colou
+  
+  ellipse( 860, 120, 60, 60);
+  c=color('#7B68EE');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 950, 120, 60, 60);
+  c=color('#F5DEB3');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 770, 120, 60, 60);
+  c=color('#FF6347');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 680, 120, 60, 60);
+  c=color('#D8BFD8');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 590, 120, 60, 60);
+  c=color('#708090');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 500, 120, 60, 60);
+  c=color('#FFFF00');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 120, 60, 60);
+  c=color('#F4A460');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 120, 60, 60);
+  c=color('#FF0000');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 120, 60, 60);
+  c=color('#F0E68C');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 140, 120, 60, 60);
+  c=color('#4B0082');
+  fill(c); // Use updated 'c' as fill colour
+ 
+  ellipse( 50, 120, 60, 60);
+  c=color('#FF69B4');
+  fill(c); // Use updated 'c' as fill colour
+  
+
+  ellipse( 140, 205, 60, 60);
+  c=color('#000000');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 50, 205, 60, 60);
+  c=color('#8A2BE2');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 205, 60, 60);
+  c=color('#F0F8FF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 205, 60, 60);
+  c=color('#7FFFD4');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 205, 60, 60);
+  c=color('#6495ED');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 205, 60, 60);
+  c=color('#FF8C00');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 205, 60, 60);
+  c=color('#F0FFFF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 50, 205, 60, 60);
+  c=color('#BDB76B');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 140, 205, 60, 60);
+  c=color('#008000');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 205, 60, 60);
+  c=color('#808080');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 500, 205, 60, 60);
+  c=color('#ADD8E6');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 590, 205, 60, 60);
+  c=color('#708090');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 680, 205, 60, 60);
+  c=color('#C71585');
+  fill(c); // Use updated 'c' as fill colour
+  
+   ellipse( 770, 205, 60, 60);
+  c=color('#4682B4');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 950, 205, 60, 60);
+  c=color('#EE82EE');
+  fill(c); // Use updated 'c' as fill colour
+
+  ellipse( 860, 205, 60, 60);
+  c=color('#9ACD32');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 140, 290, 60, 60);
+  c=color('#F0F8FF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 50, 290, 60, 60);
+  c=color('#FAEBD7');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 290, 60, 60);
+  c=color('#00FFFF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 290, 60, 60);
+  c=color('#7FFFD4');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 290, 60, 60);
+  c=color('#F0FFFF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 500, 290, 60, 60);
+  c=color('#F5F5DC');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 590, 290, 60, 60);
+  c=color('#FFE4C4');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 680, 290, 60, 60);
+  c=color('#000000');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 770, 290, 60, 60);
+  c=color('#FFEBCD');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 950, 290, 60, 60);
+  c=color('#0000FF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 860, 290, 60, 60);
+  c=color('#8A2BE2');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 140, 375, 60, 60);
+  c=color('#A52A2A');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 50, 375, 60, 60);
+  c=color('#DEB887');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 375, 60, 60);
+  c=color('#5F9EA0');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 375, 60, 60);
+  c=color('#7FFF00');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 375, 60, 60);
+  c=color('#D2691E');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 500, 375, 60, 60);
+  c=color('#FF7F50');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 590, 375, 60, 60);
+  c=color('#6495ED');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 680, 375, 60, 60);
+  c=color('#FFF8DC');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 770, 375, 60, 60);
+  c=color('#DC143C');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 860, 375, 60, 60);
+  c=color('#00FFFF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 950, 375, 60, 60);
+  c=color('#00008B');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 140, 460, 60, 60);
+  c=color('#008B8B');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 50, 460, 60, 60);
+  c=color('#B8860B');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 460, 60, 60);
+  c=color('#A9A9A9');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 460, 60, 60);
+  c=color('#006400');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 460, 60, 60);
+  c=color('#BDB76B');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 500, 460, 60, 60);
+  c=color('#8B008B');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 590, 460, 60, 60);
+  c=color('#556B2F');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 680, 460, 60, 60);
+  c=color('#FF8C00');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 770, 460, 60, 60);
+  c=color('#9932CC');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 860, 460, 60, 60);
+  c=color('#1E90FF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  
+  ellipse( 950, 460, 60, 60);
+  c=color('#8B0000');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 140, 545, 60, 60);
+  c=color('#E9967A');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 50, 545, 60, 60);
+  c=color('#8FBC8F');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 230, 545, 60, 60);
+  c=color('#483D8B');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 320, 545, 60, 60);
+  c=color('#2F4F4F');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 410, 545, 60, 60);
+  c=color('#2F4F4F');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 500, 545, 60, 60);
+  c=color('#00CED1');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 590, 545, 60, 60);
+  c=color('#9400D3');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 680, 545, 60, 60);
+  c=color('#FF1493');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 770, 545, 60, 60);
+  c=color('#00BFFF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 860, 545, 60, 60);
+  c=color('#1E90FF');
+  fill(c); // Use updated 'c' as fill colour
+  
+  ellipse( 950, 545, 60, 60);
+  c=color('#696969');
+  fill(c); // Use updated 'c' as fill colour
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
